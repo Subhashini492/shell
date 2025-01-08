d7ca9adb70a29fee27e915946d6624b6598dbb28
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-08 19:29:20 +0530

    added files

--- web_ubantu.sh
+++ web_ubantu.sh
@@ -6,6 +6,6 @@ sudo systemctl enable nginx
 sudo rm -rf /var/www/html/*
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
 cd /var/www/html
-sudo apt install unzip
+sudo apt install unzip -y
 unzip /tmp/web.zip
 systemctl restart nginx 
