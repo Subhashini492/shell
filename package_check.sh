05978c3b198f1db6de76766f72dd2e709ca3eb92
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-09 15:37:08 +0530

    added user

--- package_check.sh
+++ package_check.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-sudo apt list --installed | grep $1 $2 $3
-if [ $? -eq 0 ];
-then
-    echo "$1 installed"
-else
-    echo "$1 not installed"
-fi
+
+
+for packages in $#
+do
+    sudo apt list --installed | grep $packages
+    if [ $? -eq 0 ];
+    then
+        echo "$packages installed"
+    else
+        echo "$packages not installed"
+    fi
+done
+
+
