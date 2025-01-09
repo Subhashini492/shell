7b1df947e6a49946d6c8ab396c1ce50f585af6bf
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-09 13:25:22 +0530

    added user

--- if.sh
+++ if.sh
@@ -3,7 +3,7 @@ date +%x
 ID=(id -u) 
 if [ $ID -eq 0 ];
 then 
-    echo "you are root user . good to go"
+    echo "you are root user . good to go , thank you"
 else
     echo " you are not a root user"
 fi
