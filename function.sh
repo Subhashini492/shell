765c1ca0930f14ecb598d9369c3b99db00ca8d79
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-09 16:31:27 +0530

    added user

---
+++ function.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+validation () {
+    if [ $1 -eq 0 ];
+    then
+    echo "SUCUESS $2"
+    else
+        echo "FAILED $2"
+        exit 1
+    fi
+}
+
+ls -la
+validation $? "ls -la"
+
+pwd
+validation $? "pwd"
+
+pwf
+validation $? "pwf"
+
+ls
+validation $? "ls"
