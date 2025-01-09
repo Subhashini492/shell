66d1b4946b728b588a1c87fcba1111d04b5280ca
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-09 14:35:24 +0530

    added user

--- For.sh
+++ For.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for peru in subbu sirisha sai charan venkatesh
-do 
-    echo "name : $peru"
-done
+# for peru in subbu sirisha sai charan venkatesh
+# do 
+#     echo "name : $peru"
+# done
 
 for server in tomcat nginx httpd 
 do 
