ac58e5cfb108572a3d76b9c9248e0a5fafaf400e
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-11 15:27:46 +0530

    added user

--- web.sh
+++ web.sh
@@ -18,28 +18,28 @@ validate () {
         exit 1
     fi
 }
-apt install nginx -y &>>/tmp/$0
+apt install nginx -y &>>/tmp/web.log
 validate $? "install nginx"
 
-systemctl start nginx &>>/tmp/$0
+systemctl start nginx &>>/tmp/web.log
 validate $? "start nginx"
 
-systemctl enable nginx &>>/tmp/$0
+systemctl enable nginx &>>/tmp/web.log
 validate $? "enable nginx"
 
-rm -rf /var/www/html/* &>>/tmp/$0
+rm -rf /var/www/html/* &>>/tmp/web.log
 validate $? "defult html files deleted"
 
-curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>>/tmp/$0
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>>/tmp/web.log
 validate $? "downloading artifact in tmp folder"
 
-apt install unzip -y &>>/tmp/$0
+apt install unzip -y &>>/tmp/web.log
 validate $? "installed unzip"
 
-unzip /tmp/web.zip -d /var/www/html/* &>>/tmp/$0
+unzip /tmp/web.zip -d /var/www/html/* &>>/tmp/web.log
 validate $? "unzipping web"
 
-systemctl restart nginx &>>/tmp/$0
+systemctl restart nginx &>>/tmp/web.log
 validate $? "restarting nginx"
 
 
