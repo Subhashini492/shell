5367423f822aff786628774c787bc4a1e351f1a7
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-11 15:15:44 +0530

    added user

--- web.sh
+++ web.sh
@@ -6,3 +6,49 @@ then
 else 
     echo "please run the code with root user"
 fi 
+
+apt update
+validate () {
+    if [ $1 -eq 0 ];
+    then 
+        echo "$2 SUCCESS"
+    else 
+        echo "$2 FAILURE"
+        exit 1
+    fi
+}
+apt install nginx -Y &>>/tmp/$0
+validate $? "install nginx"
+
+systemctl start nginx &>>/tmp/$0
+validate $? "start nginx"
+
+systemctl enable nginx &>>/tmp/$0
+validate $? "enable nginx"
+
+rm -rf /var/www/html/* &>>/tmp/$0
+validate $? "defult html files deleted"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>>/tmp/$0
+validate $? "downloading artifact in tmp folder"
+
+apt install unzip -y &>>/tmp/$0
+validate $? "installed unzip"
+
+unzip /tmp/web.zip -d /var/www/html/* &>>/tmp/$0
+validate $? "unzipping web"
+
+systemctl restart nginx &>>/tmp/$0
+validate $? "restarting nginx"
+
+
+
+
+
+
+
+
+
+
+
+ 
