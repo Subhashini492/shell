0516cb4cc5570a93f6e6395c31eefac6bb5e5dde
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-14 16:03:18 +0530

    added user

--- user.sh
+++ user.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 echo "welcome to the game"
 read -p "please enter user_name:" user
+if [ $user -eq "subhashini" ];
+then
+    
+else
+    echo "please enter valid user name"
+    exit 1
+fi
 read -p "please enter password:" password
+if [ $password -eq "subbu@123" ];
+then
+else 
+    echo "please enter correct password"
+    exit 1
+fi
 
+echo "this is your user_name :$user  & password :$password"
 
