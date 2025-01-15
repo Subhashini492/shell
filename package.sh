232f21a8aa959befb3bea4a300d51c55ae1b3920
Subhashini492 <idamakantibangaram567@gmail.com>
2025-01-15 19:52:06 +0530

    added user

--- package.sh
+++ package.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-id=$(yum list installed | grep package_name)
-if [ $? -eq 0 ]
-then
-    echo " git installed skipped installed "
+PACKAGE_NAME="git"
 
-else 
-    echo " sudo git installed "
-    exit 1
+if yum list installed "$PACKAGE_NAME" &>/dev/null; 
+then
+    echo "Package '$PACKAGE_NAME' is already installed. Skipping installation."
+else
+    echo "Package '$PACKAGE_NAME' is not installed. Attempting to install it..."
+    sudo yum install -y "$PACKAGE_NAME"
+
+    if [ $? -eq 0 ]; 
+    then
+        echo "Package '$PACKAGE_NAME' installed successfully."
+    else
+        echo "Failed to install package '$PACKAGE_NAME'."
+        exit 1
+    fi
 fi
